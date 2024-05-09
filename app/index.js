1277b7e5371909e511d7b098b111b5a4349321d8
Shiham123 <s36651786@gmail.com>
2024-05-09 10:41:32 +0600

    server initialize

--- app/index.js
+++ app/index.js
@@ -1,1 +1,29 @@
-console.log('hello eslint');
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
+// application port
+
+/* eslint-disable-next-line no-undef */
+const port = process.env.PORT
+
+// routes
+app.get("/", (req, res) => {
+	try {
+		res.json("get request")
+	} catch (error) {
+		res.json(error)
+	}
+})
+
+app.listen(port, () => {
+	/* eslint-disable no-console */
+	console.log(`server running at http://localhost:${port}`)
+})
