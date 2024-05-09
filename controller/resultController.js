eed73b2e38d22a079baadb84a4ca4677d91bbf35
Shiham123 <s36651786@gmail.com>
2024-05-09 20:07:47 +0600

    question router and result route created successfully

--- controller/resultController.js
+++ controller/resultController.js
@@ -6,4 +6,20 @@ const getAllResult = (req, res) => {
 	}
 }
 
-module.exports = {getAllResult}
+const postAllResult = (req, res) => {
+	try {
+		return res.status(200).json({message: "posted all result"})
+	} catch (error) {
+		return res.status(200).json({message: error.message})
+	}
+}
+
+const deleteResult = (req, res) => {
+	try {
+		return res.status(200).json({message: "delete result here"})
+	} catch (error) {
+		return res.status(400).json({message: error.message})
+	}
+}
+
+module.exports = {getAllResult, postAllResult, deleteResult}
--- router/route.js
+++ router/route.js
@@ -3,12 +3,12 @@ const router = Router()
 
 /** imported controller */
 const {getAllQuestion, postInsertQuestions, deleteAllQuestion} = require("../controller/question")
-const {getAllResult} = require("../controller/resultController")
+const {getAllResult, postAllResult, deleteResult} = require("../controller/resultController")
 
 /** question route */
 router.route("/questions").get(getAllQuestion).post(postInsertQuestions).delete(deleteAllQuestion)
 
 /** Result router */
-router.route("/result").get(getAllResult)
+router.route("/result").get(getAllResult).post(postAllResult).delete(deleteResult)
 
 module.exports = router
