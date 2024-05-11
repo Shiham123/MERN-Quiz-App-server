c569bc92d9c0fcdba2a5751838215f23d31fd5e8
Shiham123 <s36651786@gmail.com>
2024-05-11 10:33:58 +0600

    result create get and delete working perfectly

--- src/app/secret.js
+++ src/app/secret.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const portNumber = process.env.PORT || 5000
 
 const mongodbURL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster-one.varjcyv.mongodb.net/MernQuizApp`
-const mongodbLocalUrl = `mongodb://localhost:27017`
+const mongodbLocalUrl = `mongodb://localhost:27017/MernQuizApp`
 
 module.exports = {portNumber, mongodbURL, mongodbLocalUrl}
